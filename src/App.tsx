1743d96da2a419bb3005cf2afe37ac4e20298555
Alex Bihas <alex.bigas.2004@gmail.com>
2024-03-09 21:11:15 +0200

    repo init

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { useState } from 'react';
+import loader from './assets/loader.svg';
+import './App.css';
+
+function App() {
+  const [images, setImages] = useState<string[]>([]);
+
+  return <>Hello world</>;
+}
+
+export default App;
